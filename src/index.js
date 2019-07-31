88a92974616041aa177b889da9f3afcd098ae382
Ryan Morr <rm.morr@gmail.com>
2019-07-31 14:35:19 -0400

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-export default function fizzbuzz(n) {
-    if (n % 15 === 0) {
-        return 'FizzBuzz';
-    } else if (n % 5 === 0) {
-        return 'Buzz';
-    } else if (n % 3 === 0) {
-        return 'Fizz';
-    }
-    return n;
+export default function foo() {
+    return 'foo';
 }
--- test/specs/index.js
+++ test/specs/index.js
@@ -1,29 +1,12 @@
-import fizzbuzz from '../../src/index';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import foo from '../../src/index';
 
-describe('fizzbuzz', () => {
-    it('returns \'Fizz\' when the given param is a multiplier of 3', () => {
-        expect(fizzbuzz(3)).to.equal('Fizz');
-        expect(fizzbuzz(6)).to.equal('Fizz');
-        expect(fizzbuzz(9)).to.equal('Fizz');
-    });
+describe('foo', () => {
+    it('should return foo', () => {
+        const fooSpy = sinon.spy(foo);
 
-    it('returns \'Buzz\' when the given param is a multiplier of 5', () => {
-        expect(fizzbuzz(5)).to.equal('Buzz');
-        expect(fizzbuzz(10)).to.equal('Buzz');
-        expect(fizzbuzz(20)).to.equal('Buzz');
-    });
-
-    it('returns \'FizzBuzz\' when the given param is a multiplier of 3 and 5', () => {
-        expect(fizzbuzz(15)).to.equal('FizzBuzz');
-        expect(fizzbuzz(30)).to.equal('FizzBuzz');
-        expect(fizzbuzz(45)).to.equal('FizzBuzz');
-    });
-
-    it('returns the given number when the number is not a multiplier of 3 or 5', () => {
-        expect(fizzbuzz(1)).to.equal(1);
-        expect(fizzbuzz(2)).to.equal(2);
-        expect(fizzbuzz(4)).to.equal(4);
-        expect(fizzbuzz(7)).to.equal(7);
-        expect(fizzbuzz(8)).to.equal(8);
+        expect(fooSpy()).to.equal('foo');
+        expect(fooSpy.called).to.equal(true);
     });
 });
